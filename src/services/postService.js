a16624be07f579fa283cb3db87e6ffe0ff180751
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-10-27 11:40:01 -0300

    Requisito 12: Fixed

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const { BlogPost, PostCategory } = require('../models');
+const { BlogPost, PostCategory, Category } = require('../models');
 const { baseError, codes } = require('../utils/baseError');
 
 const config = require('../config/config');
@@ -8,25 +8,36 @@ const config = require('../config/config');
 const env = process.env.NODE_ENV || 'development';
 const sequelize = new Sequelize(config[env]);
 
+const verifyCategoryIds = async (categoryIds) => {
+  if (!categoryIds.length) return false;
+  const categories = await Promise.all(categoryIds.map((categoryId) => (
+    Category.findOne({ where: { id: categoryId } })
+  )));
+  if (categories.includes(null)) return false;
+  return true;
+};
+
 const create = async (user, postData) => {
   try {
     const result = await sequelize.transaction(async (t) => {
       const { id: userId } = user;
       const { title, content, categoryIds } = postData;
+
+      if (!(await verifyCategoryIds(categoryIds))) {
+        return baseError(codes.BAD_REQUEST, 'one or more "categoryIds" not found');
+      }
+
       const postResult = await BlogPost.create({
-        title, content, userId, published: new Date(), updated: new Date(),
-      }, { transaction: t });
+        title, content, userId, published: new Date(), updated: new Date() }, { transaction: t });
       const post = { ...postResult.dataValues, id: postResult.null };
       await Promise.all(categoryIds.map((categoryId) => (
-        PostCategory.create({ postId: post.id, categoryId }, { transaction: t })
-      )));
+        PostCategory.create({ postId: post.id, categoryId }, { transaction: t }))));
 
       return post;
     });
     return result;
   } catch (e) {
-    console.error(e);
-    return baseError(codes.BAD_REQUEST, 'one or more "categoryIds" not found');
+    return baseError(codes.INTERNAL_SERVER_ERROR, 'Internal Server Error');
   }
 };
 
